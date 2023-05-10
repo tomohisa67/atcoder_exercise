af5343063d14ad101b5e013c9bdeb245ab774b1c
TabuchiTomohisa <56678149+TabuchiTomohisa@users.noreply.github.com>
2023-05-10 23:40:06 +0900

    add files

--- A14.py
+++ A14.py
@@ -16,3 +16,40 @@ B = list(map(int, input().split()))
 C = list(map(int, input().split()))
 D = list(map(int, input().split()))
 
+def binary_search(x, sum):
+    left = 0
+    right = N - 1   
+    while left < right:
+        mid = (left + right) // 2
+        sum += x[mid]
+        if sum > K:
+            right = mid
+        elif sum < K:
+            left = mid + 1
+        else:
+            return True
+    return False
+
+left = 0
+right = N - 1
+b = B[0]
+c = C[0]
+d = D[0]
+for a in A:
+    for b in B:
+        for c in C:
+            # dの処理
+            while left < right:
+                mid = (left + right) // 2
+                if sum > K:
+                    right = mid
+                elif sum < K:
+                    left = mid + 1
+                else:
+                    print("Yes")
+                    exit()
+            if binary_search(d, sum):
+                print("Yes")
+                exit()
+print("No")
+
--- A15.py
+++ A15.py
@@ -1,6 +1,6 @@
 import io
 import sys
-
+import numpy as np
 _INPUT = """\
 5
 46 80 11 77 46
@@ -9,3 +9,33 @@ sys.stdin = io.StringIO(_INPUT)
 
 N = int(input())
 A = list(map(int, input().split()))
+A = np.array(A)
+B = np.array([], dtype=int)
+
+# print(A)
+I = np.argsort(A)
+# print(I)
+A = A[I]
+# print(A)
+A_old = -1
+num = 0
+for i in range(N):
+    if A_old != A[i]:
+        num += 1
+    B = np.append(B, num)
+    A_old = A[i]
+# print(B)
+# B = B[I]
+# for i in range(N):
+#     B
+b0 = B[0].copy()
+b1 = B[1].copy()
+b2 = B[2].copy()
+b3 = B[3].copy()
+b4 = B[4].copy()
+B[0] = b1
+B[1] = b4
+B[2] = b0
+B[3] = b3
+B[4] = b2
+print(B)
